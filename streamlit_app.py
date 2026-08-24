ff5502bde5258ccdc524202cfb88d9f21e6dd7f0
Yashwanth Karumanchi <75605054+Yashwanth-Karumanchi@users.noreply.github.com>
2026-08-24 11:51:03 -0600

    Fix ingredients string concatenation

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = += fruit_chosen
+    ingredients_string = " ".join(ingredients_list)
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
